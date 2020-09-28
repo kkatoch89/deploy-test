ad52488217d341548636eb2cdde5388dfe51cae6
kkatoch89 <karankatoch.hba@gmail.com>
2020-09-28 12:21:14 -0400

    Updates

---
+++ precache-manifest.1b7684b5af7ee5b0da1700b7336bf087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ff2a844a028d1cca32ecbedcb786d3",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "34b361e181f62bb1abc0",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79d5610f2beb4a3884bb",
+    "url": "/deploy-test/static/js/2.0d2750ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-test/static/js/2.0d2750ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b361e181f62bb1abc0",
+    "url": "/deploy-test/static/js/main.958e5ea3.chunk.js"
+  },
+  {
+    "revision": "e9c2fd66a894ae6d6e06",
+    "url": "/deploy-test/static/js/runtime-main.7dc19a72.js"
+  }
+]);
